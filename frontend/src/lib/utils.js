322579b10636b73ce4ff28d8e0f4c5aba784871a
Anujshivhare <143898263+Anujshivhare@users.noreply.github.com>
2024-07-14 00:37:29 +0530

    Update utils.js

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -64,3 +64,20 @@ export function getRelatedPost(post, posts) {
 
   return relatedPosts;
 }
+
+  const options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  const date = new Date(dateString);
+  const formattedDate = date.toLocaleDateString("en-US", options);
+
+  const time = date.toLocaleTimeString([], {
+    hour: "numeric",
+    minute: "2-digit",
+  });
+
+  return `${formattedDate} at ${time}`;
+}
